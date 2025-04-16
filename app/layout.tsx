31a092b6e8e59a3bda40b642d805e73d079bec9b
ADITYA <adityah.1si21cs002@gmail.com>
2025-04-16 14:46:20 +0530

    feat: initialize Next.js project with TypeScript and Tailwind CSS
    - Added package.json with dependencies for React, Next.js, TypeScript, and Tailwind CSS.
    - Created postcss.config.mjs for Tailwind CSS integration.
    - Added SVG assets for icons and branding (file.svg, globe.svg, next.svg, vercel.svg, window.svg).
    - Configured TypeScript with tsconfig.json for strict type checking and module resolution.

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+// --important--
+import "./globals.css";
+
+import React ,{PropsWithChildren} from 'react'
+
+const RootLayout = (props:PropsWithChildren) => {
+  return (
+    <html>
+      <body>
+        {props.children}
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
