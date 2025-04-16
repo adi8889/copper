349df47e246a69baf479e8718e3c74445b4adab5
ADITYA <adityah.1si21cs002@gmail.com>
2025-04-16 15:25:32 +0530

    feat: implement NavigationBar component and add Hono SVG asset

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import NavigationBar from '@/components/navigation-bar/NavigationBar';
 import React from 'react'
 
 const Rootpage = () => {
-  return (
-    <div>page</div>
-  )
-}
+  return <div className='flex flex-col items-stretch'>
+  
+  <NavigationBar/>
+  <div className='h-48 bg-orange-500'/>
+  <div className='h-48 bg-purple-500'/>
+  <div className='h-28 bg-green-500'/>
+
+  </div>
+
+
+};
 
 export default Rootpage;
